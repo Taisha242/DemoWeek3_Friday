82bbdb85126e77fd405ccaf32c8b7f9165fa6470
taish <taish@10.0.0.164>
2023-05-29 14:29:19 -0400

    initial commit

---
+++ src/main/java/com/mycompany/week3/Week3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.week3;
+
+/**
+ *
+ * @author taish
+ */
+public class Week3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
